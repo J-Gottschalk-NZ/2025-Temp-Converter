7ad575f54d225098e5f2e8131b76f935b8842ee1
J-Gottschalk-NZ <49209767+J-Gottschalk-NZ@users.noreply.github.com>
2025-01-17 12:11:37 +1300

    Working Converter component

--- conversion_rounding.py
+++ conversion_rounding.py
@@ -1,6 +1,8 @@
 def round_ans(val):
     """
-    Rounds floats UP to nearest integer
+    Rounds temperatures to nearest degree
+    :param val: Number to be rounded
+    :return: Number rounded to nearest degree
     """
     var_rounded = (val * 2 + 1) // 2
     return "{:.0f}".format(var_rounded)
@@ -8,7 +10,9 @@ def round_ans(val):
 
 def to_celsius(to_convert):
     """
-    Converts from degrees °F to °C
+    Converts from °F to °C
+    :param to_convert: Temperature to be converted in °F
+    :return:  Converted temperature in °C
     """
     answer = (to_convert - 32) * 5 / 9
     return round_ans(answer)
@@ -16,19 +20,24 @@ def to_celsius(to_convert):
 
 def to_fahrenheit(to_convert):
     """
-    Converts from °F to °C
+    Converts from °C to °F
+    :param to_convert: Temperature to be converted in °C
+    :return:  Converted temperature in °F
     """
     answer = to_convert * 1.8 + 32
     return round_ans(answer)
 
 
-to_c_test = [0, 100, -459]
-to_f_test = [0, 100, 40, -273]
-
-for item in to_f_test:
-    print(f"{item} C is {to_fahrenheit(item)} F")
-
-print()
-
-for item in to_c_test:
-    print(f"{item} F is {to_celsius(item)} C")
+# Main Routine / Testing starts here
+# to_c_test = [0, 100, -459]
+# to_f_test = [0, 100, 40, -273]
+#
+# for item in to_f_test:
+#     ans = to_fahrenheit(item)
+#     print(f"{item} C is {ans} F")
+#
+# print()
+#
+# for item in to_c_test:
+#     ans = to_celsius(item)
+#     print(f"{item} F is {ans} C")
