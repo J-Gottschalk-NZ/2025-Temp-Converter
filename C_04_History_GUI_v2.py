ed05e68e257fde663c153adb753cf1018599cd07
J-Gottschalk-NZ <49209767+J-Gottschalk-NZ@users.noreply.github.com>
2025-01-19 14:09:37 +1300

    Working History (does not write to file)

--- C_04_History_GUI_v2.py
+++ C_04_History_GUI_v2.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from functools import partial  # To prevent unwanted windows
+import all_constants as c
 
 
 class Converter:
@@ -15,6 +16,14 @@ class Converter:
         self.temp_frame = Frame(padx=10, pady=10)
         self.temp_frame.grid()
 
+        # self.calculations_list = ['10.0 °F is -12°C', '20.0 °F is -7°C',
+        #                           '30.0 °F is -1°C', '40.0 °F is 4°C',
+        #                           '50.0 °F is 10°C', '60.0 °F is 16°C']
+
+        self.calculations_list = ['10.0 °F is -12°C', '20.0 °F is -7°C',
+                                  '30.0 °F is -1°C', '40.0 °F is 4°C',
+                                  '50.0 °F is 10°C']
+
         self.to_history_button = Button(self.temp_frame,
                                         text="History / Export",
                                         bg="#CC6600",
@@ -28,7 +37,7 @@ class Converter:
         Opens history dialogue box and disables history button
         (so that users can't create multiple history boxes).
         """
-        HistoryExport(self)
+        HistoryExport(self, self.calculations_list)
 
 
 class HistoryExport:
@@ -36,12 +45,19 @@ class HistoryExport:
     Displays history dialogue box
     """
 
-    def __init__(self, partner):
+    def __init__(self, partner, calculations):
         # setup dialogue box and background colour
 
-        green_back = "#D5E8D4"
-        peach_back = "#ffe6cc"
+        # Work out background colour for calculations and create intro label
+        if len(calculations) <= c.MAX_CALCS:
+            calc_back = "#D5E8D4"
+            calc_amount = "all your"
+        else:
+            calc_back = "#ffe6cc"
+            calc_amount = f"your recent calculations - showing {c.MAX_CALCS} / {len(calculations)}"
 
+        recent_intro_txt = (f"Below are {calc_amount} calculations "
+                            "(to the nearest degree).")
 
         self.history_box = Toplevel()
 
@@ -56,21 +72,38 @@ class HistoryExport:
         self.history_frame = Frame(self.history_box)
         self.history_frame.grid()
 
-        # strings for 'long' labels...
-        recent_intro_txt = ("Below are your recent calculations - showing "
-                            "3 / 3 calculations.  All calculations are "
-                            "shown to the nearest degree")
+        # strings for 'long' label      s...
 
         export_instruction_txt = ("Please push <Export> to save your calculations in a text "
                                   "file.  If the filename already exists, it will be overwritten!")
 
-        calculations = ""
+        newest_first_list = list(reversed(calculations))
+        print(newest_first_list)
+
+        # create calculations as a string (most recent five calculations).
+        newest_first_string = ""
+
+        # create string of entire list if we
+        # have five or fewer calculations
+
+        if len(newest_first_list) <= c.MAX_CALCS:
+
+            for item in newest_first_list[:-1]:
+                newest_first_string += item + "\n"
+
+            newest_first_string += newest_first_list[-1]
+
+        else:
+            for item in newest_first_list[:4]:
+                newest_first_string += item + "\n"
+
+            newest_first_string += newest_first_list[4]
 
         # Label list (label text | format | bg)
         history_labels_list = [
             ["History / Export", ("Arial", "16", "bold"), None],
             [recent_intro_txt, ("Arial", "11"), None],
-            ["calculation list", ("Arial", "14"), green_back],
+            [newest_first_string, ("Arial", "14"), calc_back],
             [export_instruction_txt, ("Arial", "11"), None]
         ]
 
@@ -90,8 +123,6 @@ class HistoryExport:
         # make frame to hold buttons (two columns)
         self.hist_button_frame = Frame(self.history_box)
         self.hist_button_frame.grid(row=4)
-
-        button_ref_list = []
 
         # button list (button text | bg colour | command | row | column)
         button_details_list = [
